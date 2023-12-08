a1c389d75817ea1460a9e6f72b71b4407ace88b8
Iamnotbao <ohshit30102015@gmail.com>
2023-12-08 21:54:37 +0700

    update

---
+++ Spotify_Run/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from 'react-native';
+import App_Run from "./src/App";
+
+const App = () => {
+  return (
+    <View>
+      <App />
+    </View>
+  )
+}
---
+++ Spotify_Run/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native"
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const App_Run = () => {
+    return (
+        <View><Text>App run...</Text></View>
+    )
+}
+export default App_Run;
