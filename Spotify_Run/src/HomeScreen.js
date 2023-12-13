5f50c5fabf7d4a53870d09cc3ae7abc7bf0133ba
Iamnotbao <ohshit30102015@gmail.com>
2023-12-13 11:43:08 +0700

    Update

--- Spotify_Run/src/HomeScreen.js
+++ Spotify_Run/src/HomeScreen.js
@@ -1,65 +1,106 @@
 import React, { useEffect, useState } from "react";
-import { View, Text,ScrollView  } from "react-native"
+import { View, Text, Image, ActivityIndicator, TouchableOpacity } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
-import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
 
 
 const HomeScreen = () => {
- const [data,setData] = useState([]);
- const[isLoading,setIsLoading]= useState(true);
- 
-  const fetchData =async ()=>{
-    const value= await AsyncStorage.getItem("token");
-    console.log(value);
-    try{
-  const response =  await axios.get('https://api.spotify.com/v1/me',{
-        headers:{
-            
-            Authorization: `Bearer ${value}`
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-        }
-         
-    })
-    const data_receive = await response.json();
-    isLoading(False);
-    setData(data_receive);
-    return(data_receive);
-
-    } catch (err) {
+  const fetchData = async () => {
+    const value = await AsyncStorage.getItem('token');
+    if (value !== null) {
+      try {
+        const response = await fetch('https://api.spotify.com/v1/me', {
+          headers: {
+            Authorization: `Bearer ${value}`,
+          },
+        });
+        const data_receive = await response.json();
+        setData(data_receive);
+        setIsLoading(false);
+      } catch (err) {
         console.log(err.message);
         if (err.response && err.response.status === 401) {
-          // Handle 401 Unauthorized error
           console.log('Token may be expired or invalid. Please re-authenticate.');
         }
+        setIsLoading(false);
+      }
     }
-}
+  };
+  const message = () => {
+    const presentTime = new Date().getHours();
+    if (presentTime < 12) {
+      return "Ohayo (~^ 3 ^~)";
+    } else if (presentTime < 17) {
+      return "Konichiwa (~T v T~)";
+    } else {
+      return "Konbanwa (~V . V~)"
+    }
+  }
 
-
-
-  useEffect(()=>{
+  const message_User = message();
+  useEffect(() => {
     fetchData();
-  },[])
-  console.log(data)
+  }, []);
 
+  if (isLoading) {
+    return <ActivityIndicator size="large" color="#0000ff" />;
+  }
 
+  return (
+    <LinearGradient
+      colors={["#040306", "#131624"]}
+      style={{ flex: 1 }}>
 
+      <View>
+        <View style={{ flexDirection: "row", alignItems: "center" , justifyContent:"space-between"}}>
+          {data && data.images && data.images.length > 0 && (
+            <Image
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 100,
+                marginTop: 20,
+                resizeMode: "cover",
+              }}
+              source={{ uri: data.images[0].url }}
+            />
+          )
+          }
+          <Text style={{ color: 'white', marginLeft: 5, fontSize: 16, fontWeight: 'bold', marginTop: 20, }}>{message_User}</Text>
+          <MaterialCommunityIcons  style={{marginLeft:220, marginTop:20}} name="lightning-bolt-outline" size={24} color="white" />
+        </View>
+       <View style={{marginHorizontal:12, marginVertical:5, flexDirection:'row',alignItems:'center',gap:10}}> 
+        <TouchableOpacity
+        style={{
+          backgroundColor:"yellow"
+          ,fontWeight:'bold',borderRadius:30,padding:10,
 
-    return (
-        <LinearGradient
-            colors={["#040306", "#131624"]}
-            style={{ flex: 1 }}>
-                <ScrollView style={{marginTop:50}}>
-                    <Text style={{color:"white"}}>Test ok</Text>
+        }}  
+        >
+          <Text style={{color:'black'}}>Music</Text>
+        </TouchableOpacity>
 
+        <TouchableOpacity
+        style={{
+          backgroundColor:"yellow"
+          ,fontWeight:'bold',borderRadius:30,padding:10,
 
-                </ScrollView>
+        }}  
+        >
+          <Text style={{color:'black'}}>Podcards & Shows</Text>
+        </TouchableOpacity>
+       </View>
+        
+      </View>
+    </LinearGradient>
+  );
+};
 
-
-
-
-        </LinearGradient>
-
-    )
-}
 export default HomeScreen;
--- Spotify_Run/src/Login.js
+++ Spotify_Run/src/Login.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Linking } from 'react-native';
 
 import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
@@ -8,114 +9,103 @@ import { authorize } from "react-native-app-auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "@react-navigation/native";
 const Login = () => {
-    const navigation = useNavigation();
-    const qs = require('qs');
-    const Buffer = require('buffer').Buffer;
-    var client_id = '62a47492eec04acd9c378e883d88eca2';
-    var client_secret = '7c18f624d4134f14861f4059cb697055';
+  const navigation = useNavigation();
+  const [isLoading, setIsLoading] = useState(true);
+  // Configuration for Spotify OAuth
+  const config = {
+    clientId: '62a47492eec04acd9c378e883d88eca2',
+    response_type: 'code',
+    redirectUrl: 'spotifyapp://callback',
+    scopes: [
+      'user-read-email',
+      'user-library-read',
+      'user-read-recently-played',
+      'user-top-read',
+      'playlist-read-private',
+      'playlist-read-collaborative',
+      'playlist-modify-public' // or 'playlist-modify-private'
+    ],
+    serviceConfiguration: {
+      authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+      tokenEndpoint: 'https://accounts.spotify.com/api/token',
+    },
+  };
 
-    var authOptions = {
-        method: 'POST',
-        url: 'https://accounts.spotify.com/api/token',
-        headers: {
-            'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')),
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: qs.stringify({
-            grant_type: 'client_credentials'
-        })
-    };
-    async function Run() {
-        await axios(authOptions).then( async response => {
-            const token = response.data.access_token;
-           try{
-            if(token){
-            const expiration = new Date(token.accessTokenExpirationDate).getTime();
-            await AsyncStorage.setItem('token',token);
-            await AsyncStorage.setItem('expiration',expiration.toString());
-            console.log('Storage successfully!!!')
-             navigation.navigate("Main_Screen");
-            }
-           }
-           catch(error){
-            console.log('No value for Storaging!!!')
-           }
-         
-        })
-        .catch(error => {
-            console.error(error);
-
-        });
-    }
-
-useEffect(()=>{
-  const checkToken = async () =>{
-     const access_token = await AsyncStorage.getItem('token');
-     const expiration_token= await AsyncStorage.getItem('expiration');
-     console.log("access :",access_token);
-     console.log("expiration :",expiration_token);
-
-     if(access_token && expiration_token){
-        const current_time = Date.now();
-        if(current_time < parseInt(expiration_token)){
-            navigation.replace("Main_Screen");
-        }else{
-            await AsyncStorage.removeItem('token')
-            await AsyncStorage.removeItem('expiration')
+  // Function to initiate the authentication process
+  const authenticate = async () => {
+    try {
+      const result = await authorize(config);
+      console.log(result.accessToken);
+      console.log('Authentication result:', result);
+      try {
+        if (result) {
+          await AsyncStorage.setItem('token', result.accessToken);
+          console.log("Storage successfully");
+          navigation.navigate('Main_Screen');
         }
-     }
-  }
-  checkToken(); 
+      } catch (err) {
+        console.error('Error Storage:', err);
+      }
+
+    } catch (error) {
+      console.error('Authentication error:', error);
+    }
+  };
 
 
-},[])
+  // useEffect(()=>{
+  //   authenticate();
+  // },[]);
+
+  // Linking.addEventListener('url', (event) => {
+  //     const { url } = event;
+  //     const queryParams = new URLSearchParams(url.split('?')[1]);
+  //     const code = queryParams.get('code');
+  //     const state = queryParams.get('state');
+  //     console.log(code);
+  //     // Verify the state and handle the code
+  //   });
+  return (
+    <LinearGradient
+      colors={["#040306", "#131624"]}
+      style={{ flex: 1 }}>
+      <SafeAreaView>
+        <View style={{ height: 80 }} />
+        <FontAwesome style={{ textAlign: "center" }} name="spotify" size={80} color="white" />
+        <Text style={{ color: "white", fontSize: 40, fontWeight: "bold", textAlign: "center", marginTop: 40, marginVertical: 10 }}>Millions of Songs Free on Spotify</Text>
+      </SafeAreaView>
+      <View style={{ height: 80 }} />
+      <TouchableOpacity
+        onPress={authenticate}
+        style={{
+          backgroundColor: "#1DB964", padding: 10, marginLeft: "auto", marginRight: "auto", width: 300, borderRadius: 25,
+          alignItems: "center", justifyContent: "center", marginVertical: 10
+        }}>
+        <Text style={{ color: "black", fontWeight: "bold" }}>Sign In with Spotify</Text>
+      </TouchableOpacity>
 
 
+      <TouchableOpacity style={{
+        backgroundColor: "#0000FF", padding: 10, marginLeft: "auto", marginRight: "auto",
+        width: 300, borderRadius: 25, alignItems: "center", justifyContent: "center", flexDirection: "row", marginVertical: 10, borderColor: "#C0C0C0", borderWidth: 0.8
+      }}>
+        <Text style={{ color: "white", fontWeight: "500" }}>Continue with phone number</Text>
+      </TouchableOpacity>
 
+      <TouchableOpacity style={{
+        backgroundColor: "#0000FF", padding: 10, marginLeft: "auto", marginRight: "auto",
+        width: 300, borderRadius: 25, alignItems: "center", justifyContent: "center", flexDirection: "row", marginVertical: 10, borderColor: "#C0C0C0", borderWidth: 0.8
+      }}>
+        <Text style={{ color: "white", fontWeight: "500" }}>Sign In with Google</Text>
+      </TouchableOpacity>
 
-
-
-    return (
-        <LinearGradient
-            colors={["#040306", "#131624"]}
-            style={{ flex: 1 }}>
-            <SafeAreaView>
-                <View style={{ height: 80 }} />
-                <FontAwesome style={{ textAlign: "center" }} name="spotify" size={80} color="white" />
-                <Text style={{ color: "white", fontSize: 40, fontWeight: "bold", textAlign: "center", marginTop: 40, marginVertical: 10 }}>Millions of Songs Free on Spotify</Text>
-            </SafeAreaView>
-            <View style={{ height: 80 }} />
-            <TouchableOpacity
-                onPress={Run}
-                style={{
-                    backgroundColor: "#1DB964", padding: 10, marginLeft: "auto", marginRight: "auto", width: 300, borderRadius: 25,
-                    alignItems: "center", justifyContent: "center", marginVertical: 10
-                }}>
-                <Text style={{ color: "black", fontWeight: "bold" }}>Sign In with Spotify</Text>
-            </TouchableOpacity>
-
-
-            <TouchableOpacity style={{
-                backgroundColor: "#0000FF", padding: 10, marginLeft: "auto", marginRight: "auto",
-                width: 300, borderRadius: 25, alignItems: "center", justifyContent: "center", flexDirection: "row", marginVertical: 10, borderColor: "#C0C0C0", borderWidth: 0.8
-            }}>
-                <Text style={{ color: "white", fontWeight: "500" }}>Continue with phone number</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={{
-                backgroundColor: "#0000FF", padding: 10, marginLeft: "auto", marginRight: "auto",
-                width: 300, borderRadius: 25, alignItems: "center", justifyContent: "center", flexDirection: "row", marginVertical: 10, borderColor: "#C0C0C0", borderWidth: 0.8
-            }}>
-                <Text style={{ color: "white", fontWeight: "500" }}>Sign In with Google</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={{
-                backgroundColor: "#0000FF", padding: 10, marginLeft: "auto", marginRight: "auto",
-                width: 300, borderRadius: 25, alignItems: "center", justifyContent: "center", flexDirection: "row", marginVertical: 10, borderColor: "#C0C0C0", borderWidth: 0.8
-            }}>
-                <Text style={{ color: "white", fontWeight: "500" }}>Sign In with Facebook</Text>
-            </TouchableOpacity>
-        </LinearGradient>
-    )
+      <TouchableOpacity style={{
+        backgroundColor: "#0000FF", padding: 10, marginLeft: "auto", marginRight: "auto",
+        width: 300, borderRadius: 25, alignItems: "center", justifyContent: "center", flexDirection: "row", marginVertical: 10, borderColor: "#C0C0C0", borderWidth: 0.8
+      }}>
+        <Text style={{ color: "white", fontWeight: "500" }}>Sign In with Facebook</Text>
+      </TouchableOpacity>
+    </LinearGradient>
+  )
 }
 export default Login;
